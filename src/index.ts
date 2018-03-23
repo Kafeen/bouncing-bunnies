9cde14853161b9da8db57f625212bcb73869401c
Matthew Knowles <>
2018-03-23 22:08:55 +0000

    build(Dependencies): Added VueJS support
    Added loaders for vuejs

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+import Vue from "vue";
+
+let v: Vue = new Vue({
+    el: "#app",
+    template: `<div></div>`
+});
--- webpack.config.js
+++ webpack.config.js
@@ -2,22 +2,36 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.ts',
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/
-      }
-    ]
-  },
-  resolve: {
-    extensions: [ '.tsx', '.ts', '.js' ]
-  },
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
-  },
-  plugins: [new HtmlWebpackPlugin()]
+    entry: './src/index.ts',
+    module: {
+        rules: [{
+                test: /\.vue$/,
+                loader: 'vue-loader',
+                options: {
+                    loaders: {
+                        'scss': 'vue-style-loader!css-loader!sass-loader',
+                        'sass': 'vue-style-loader!css-loader!sass-loader?indentedSyntax',
+                    }
+                }
+            },
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+        alias: {
+            'vue$': 'vue/dist/vue.esm.js' // 'vue/dist/vue.common.js' for webpack 1
+        }
+    },
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    plugins: [new HtmlWebpackPlugin({
+        template: 'src/index.html'
+    })]
 };
